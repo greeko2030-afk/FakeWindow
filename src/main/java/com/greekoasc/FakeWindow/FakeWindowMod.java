d921e073adeb7a12f31f215b70989e1cb3ea23d8
GreekoASC <greeko2030@gmail.com>
2026-08-13 10:19:35 +0600

    Add FakeWindowMod for client mod initialization
    Implement the FakeWindowMod class for client initialization.

--- src/main/java/com/greekoasc/FakeWindow/FakeWindowMod.java
+++ src/main/java/com/greekoasc/FakeWindow/FakeWindowMod.java
@@ -1,1 +1,23 @@
+package com.greekoasc.fakewindow;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Environment(EnvType.CLIENT)
+public class FakeWindowMod implements ClientModInitializer {
+    
+    public static final String MOD_ID = "fakewindow";
+    public static final Logger LOGGER = LoggerFactory.getLogger("GreekoASC-" + MOD_ID);
+
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("Initializing FakeWindow by GreekoASC...");
+        LOGGER.info("Borderless Window and Fake Resolution Scaling Ready!");
+        
+        // You can register keybindings here later (e.g., F11 toggle)
+    }
+}
 
