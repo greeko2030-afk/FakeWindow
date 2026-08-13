1270151302e5dedb2fa305368b4e25baa5684321
GreekoASC <greeko2030@gmail.com>
2026-08-13 10:19:05 +0600

    Update WindowMixin.java

--- src/main/java/com/greekoasc/FakeWindow/mixin/WindowMixin.java
+++ src/main/java/com/greekoasc/FakeWindow/mixin/WindowMixin.java
@@ -1,1 +1,16 @@
+{
+  "required": true,
+  "minVersion": "0.8",
+  "package": "com.greekoasc.fakewindow.mixin",
+  "compatibilityLevel": "JAVA_21",
+  "mixins": [
+    "WindowMixin"
+  ],
+  "injectors": {
+    "defaultRequire": 1
+  },
+  "client": [
+    "WindowMixin"
+  ]
+}
 
