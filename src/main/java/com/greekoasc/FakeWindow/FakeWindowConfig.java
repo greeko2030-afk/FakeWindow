43c4080fed75edaee95b21f79ca900ede412dfa6
GreekoASC <greeko2030@gmail.com>
2026-08-13 10:23:38 +0600

    Create FakeWindowConfig.java

---
+++ src/main/java/com/greekoasc/FakeWindow/FakeWindowConfig.java
@@ -0,0 +1,11 @@
+package com.greekoasc.fakewindow;
+
+public class FakeWindowConfig {
+    
+    // Set your desired Fake Resolution here
+    public static int FAKE_WIDTH = 1280;
+    public static int FAKE_HEIGHT = 720;
+    
+    // Toggle for the fake resolution feature
+    public static boolean ENABLED = true;
+}
